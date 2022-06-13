5965179cfcfa799b9bf172d900d533eed18c4b00
bhure01 <dattatribhure@gmail.com>
2022-06-14 00:12:50 +0530

    Added:WebEventListener class i.e.WebDriverFireEvents to generate selenium action logs to be printed on console

--- FreeCRM_ApplicationTest/src/main/java/com/crm/util/WebEventListener.java
+++ FreeCRM_ApplicationTest/src/main/java/com/crm/util/WebEventListener.java
@@ -1,13 +1,5 @@
 package com.crm.util;
 
-/*************************************** PURPOSE **********************************
- - This class implements the WebDriverEventListener, which is included under events.
- The purpose of implementing this interface is to override all the methods and define certain useful  Log statements
- which would be displayed/logged as the application under test is being run.
- Do not call any of these methods, instead these methods will be invoked automatically
- as and when the action done (click, findBy etc).
- */
-
 import java.io.IOException;
 
 import org.openqa.selenium.By;
