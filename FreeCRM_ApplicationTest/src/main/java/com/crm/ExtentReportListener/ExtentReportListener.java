f95f0fc190b651c6b11c271e33231f407806cba4
bhure01 <dattatribhure@gmail.com>
2022-06-13 23:39:09 +0530

    Added: Dependency for extent reports

--- FreeCRM_ApplicationTest/src/main/java/com/crm/ExtentReportListener/ExtentReportListener.java
+++ FreeCRM_ApplicationTest/src/main/java/com/crm/ExtentReportListener/ExtentReportListener.java
@@ -1,36 +1,29 @@
-package com.crm.EXtentReportListener;
+package com.crm.ExtentReportListener;
 
-/*
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+//import com.relevantcodes.extentreports.IReporter;
+import com.relevantcodes.extentreports.LogStatus;
+import org.testng.*;
+import org.testng.xml.XmlSuite;
+
 import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.testng.IReporter;
-import org.testng.IResultMap;
-import org.testng.ISuite;
-import org.testng.ISuiteResult;
-import org.testng.ITestContext;
-import org.testng.ITestResult;
-import org.testng.xml.XmlSuite;
-
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
-
-public class ExtentReporterNG implements IReporter {
+public class ExtentReportListener implements IReporter {
     private ExtentReports extent;
 
-    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
-                               String outputDirectory) {
-        extent = new ExtentReports(outputDirectory + File.separator
-                + "Extent.html", true);
+    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+        extent = new ExtentReports(outputDirectory + File.separator +
+                "FreeCrmExtentReport.html", true);
 
-        for (ISuite suite : suites) {
+        for(ISuite suite : suites) {
             Map<String, ISuiteResult> result = suite.getResults();
 
-            for (ISuiteResult r : result.values()) {
+            for (ISuiteResult r : result.values()){
                 ITestContext context = r.getTestContext();
 
                 buildTestNodes(context.getPassedTests(), LogStatus.PASS);
@@ -38,7 +31,6 @@ public class ExtentReporterNG implements IReporter {
                 buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
             }
         }
-
         extent.flush();
         extent.close();
     }
@@ -46,8 +38,8 @@ public class ExtentReporterNG implements IReporter {
     private void buildTestNodes(IResultMap tests, LogStatus status) {
         ExtentTest test;
 
-        if (tests.size() > 0) {
-            for (ITestResult result : tests.getAllResults()) {
+        if(tests.size() > 0) {
+            for(ITestResult result : tests.getAllResults()){
                 test = extent.startTest(result.getMethod().getMethodName());
 
                 test.setStartedTime(getTime(result.getStartMillis()));
@@ -58,11 +50,9 @@ public class ExtentReporterNG implements IReporter {
 
                 if (result.getThrowable() != null) {
                     test.log(status, result.getThrowable());
-                } else {
-                    test.log(status, "Test " + status.toString().toLowerCase()
-                            + "ed");
+                }else {
+                    test.log(status, "Test " + status.toString().toLowerCase() + "ed");
                 }
-
                 extent.endTest(test);
             }
         }
@@ -73,4 +63,6 @@ public class ExtentReporterNG implements IReporter {
         calendar.setTimeInMillis(millis);
         return calendar.getTime();
     }
-}*/
+}
+
+
